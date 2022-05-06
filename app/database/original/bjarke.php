3c0163871f692431861f827a12deb723e821f901
Omnicast <bjarke_49@hotmail.com>
2022-05-06 10:31:10 +0200

    WorkingMaybe

--- app/database/original/bjarke.php
+++ app/database/original/bjarke.php
@@ -1,10 +1,12 @@
 <?php
-
+require( 'C:\xampp\htdocs\wordpress\wp-blog-header.php' );
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
 
 global $wpdb;
+$posts = $wpdb->get_results("SELECT titel FROM wp_aa_original");
+//print_r($posts);
+http_response_code(200);
 
-$posts = $wpdb->get_results("SELECT titel FROM $wpdb->posts");
-
-// Echo the title of the first scheduled post
-   echo $posts[0]->titel;
+echo $posts[0]->titel;
 ?>
--- app/database/original/read.php
+++ app/database/original/read.php
@@ -1,39 +1,37 @@
 <?php
 
-require_once(__ROOT__.'\app\models\original.php');
+require_once('C:\xampp\htdocs\wordpress\wp-content\plugins\Aarshjul-Plugin\app\models\original.php');
 
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 global $wpdb;
-if(isset($_GET['getpdf'])){
-    getoriginal();
-}
+getoriginal();
 
 function getoriginal(){
     global $wpdb;
     // initialize object
     $original = new Original($wpdb);
-    
+
     // query products
     $results = $original->read();
-      
+
     // check if more than 0 record found
     if(sizeof($results)>0){
-      
+
         // set response code - 200 OK
         http_response_code(200);
-      
+
         // show products data in json format
         return json_encode($results);
     }
-      
+
     else{
-      
+
         // set response code - 404 Not found
         http_response_code(404);
-      
+
         // tell the user no products found
         return json_encode(array());
     }
