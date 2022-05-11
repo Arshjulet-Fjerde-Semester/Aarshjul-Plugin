2d532d0ccc2c57c0bd8a5e8bf2fa96020cdb6b5c
Omnicast <bjarke_49@hotmail.com>
2022-05-11 12:55:54 +0200

    Nye ændringer

--- app/database/original/bjarke.php
+++ app/database/original/bjarke.php
@@ -4,9 +4,22 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 global $wpdb;
-$posts = $wpdb->get_results("SELECT titel FROM wp_aa_original");
-//print_r($posts);
-http_response_code(200);
 
-echo $posts[0]->titel;
+//print_r($_POST);
+//print_r($_GET);
+//print_r($_REQUEST);
+
+//if ($_GET['method'] == 'GetAll')
+//{
+//    $posts = $wpdb->get_results("SELECT titel,originalid FROM wp_aa_original");
+//    http_response_code(200);
+
+//    echo json_encode($posts);
+//}
+    $posts = $wpdb->get_results("SELECT titel FROM wp_aa_original WHERE originalid = " . $_GET['id']);
+    //print_r($posts);
+    http_response_code(200);
+
+    echo $posts[0]->titel;
+
 ?>
